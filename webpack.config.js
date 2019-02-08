7ea0bb7c20ad2bc4e4bf9d5f4b1f24bee72988a0
Nik <alaska.rok123@gmail.com>
2019-02-08 09:30:45 +0200

    [Pr_1] add webpack, sass loader, layout finished

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+    entry: './src/index.js',
+    mode: 'development',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'js/main.js',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(sa|sc|c)ss$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'sass-loader',
+                ],
+            },
+        ],
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: "css/[name].css",
+        }),
+    ],
+};
